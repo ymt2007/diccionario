c8ffa617f8716b8dbf9ae75b35c033a45ce8f9d4
Martinez . <pm789481@gmail.com>
2026-03-05 17:17:37 -0600

    ingreso de datos

---
+++ No.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+Dictionary<string, string> usuarios = new Dictionary<string, string>();
+string opcion;
+do
+{
+    Console.WriteLine("Ingrese el usuario");
+    string usuarioing = Console.ReadLine();
+    Console.WriteLine("Ingrese la contraseña");
+    string contrase = Console.ReadLine();
+    Console.WriteLine("Desea ingresar otro usuario? (s/n)");
+    opcion = Console.ReadLine();
+    usuarios.Add(usuarioing, contrase);
+} while (opcion != "n");
