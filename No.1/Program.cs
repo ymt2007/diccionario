5669cfe81fde4610a5adf9f1ff20f2fb2cd4540d
ALUMNO <git@github.com:Aguilar-Grecia/clase-git.git>
2026-03-05 10:35:45 -0600

    pedir el ingreso del nombre e id

---
+++ No.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Dictionary<int, string> nombre = new Dictionary<int, string>();
+for(int i=0; i<=3;i++)
+{
+    Console.WriteLine("Ingrese el ID");
+    int id=int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el nombre de la persona");
+    string nombreag=Console.ReadLine();
+}
+Console.WriteLine("Datos guardados");
+
