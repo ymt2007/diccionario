700f87c54ad85b4e62b30e58833f4d15e7ac786b
Martinez . <pm789481@gmail.com>
2026-03-05 17:25:23 -0600

    imprime la contrasela ingresando el usuario

--- No.5/Program.cs
+++ No.5/Program.cs
@@ -12,3 +12,20 @@ do
     opcion = Console.ReadLine();
     usuarios.Add(usuarioing, contrase);
 } while (opcion != "n");
+Console.Clear();
+string op;
+do
+{
+    Console.WriteLine("Ingrese el usuario del cual desea ver la contraseña");
+    string buscar = Console.ReadLine(); 
+    if(usuarios.ContainsKey(buscar))
+    {
+        Console.WriteLine($"La contraseña del usuario {buscar} es: {usuarios[buscar]}");
+    }
+    else
+    {
+        Console.WriteLine("Error, Usuario no registrado");
+    }
+    Console.WriteLine("Desea verificar la contraseña de otro usuario? (s/n)");
+    op = Console.ReadLine();
+} while (op != "n");
