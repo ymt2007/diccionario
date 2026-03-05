33483fb7678d0234b0f1845268df1f4a8146b41b
Martinez . <pm789481@gmail.com>
2026-03-05 16:10:59 -0600

    verificamos que la nota exista, si si se muestra en pantalla

--- No.3/Program.cs
+++ No.3/Program.cs
@@ -7,9 +7,24 @@ do
     string nombre=Console.ReadLine();
     Console.WriteLine("Ingrese la nota del estudiante");
     double notaing=double.Parse(Console.ReadLine());
+    notas.Add(nombre,notaing);
     Console.WriteLine("Desea ingresar otra nota? (s/n)");
     opcion=Console.ReadLine();
-} while (opcion != "s");
+} while (opcion != "n");
 Console.Clear();
-Console.WriteLine("Ingrese el nombre del estudiante con la nota a buscar");
-string buscar=Console.ReadLine();
+string op;
+do
+{
+    Console.WriteLine("Ingrese el nombre del estudiante con la nota a buscar");
+    string buscar = Console.ReadLine();
+    if (notas.ContainsKey(buscar))
+    {
+        Console.WriteLine($"La nota de {buscar} es: {notas[buscar]}");
+    }
+    else
+    {
+        Console.WriteLine("No se ha registrado notas a ese nombre");
+    }
+    Console.WriteLine("Desea buscar otra nota? (s/n)");
+    op=Console.ReadLine();
+} while (op != "n");
