e13077789ac35bf74f06db369e45d8acf5ee4678
Martinez . <pm789481@gmail.com>
2026-03-05 15:27:20 -0600

    imprimimos los productos con susu codigos

--- numero2/Program.cs
+++ numero2/Program.cs
@@ -3,4 +3,7 @@ Dictionary<int, string> productos = new Dictionary<int, string>();
 productos.Add(789456, "Pan");
 productos.Add(123546, "Jamon");
 productos.Add(456852, "Queso");
-
+foreach (var item in productos)
+{
+    Console.WriteLine(item);
+}
