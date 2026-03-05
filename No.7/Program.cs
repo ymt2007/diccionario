ea9300ff040d1c780ba44ce029a5949e8bc2d976
Martinez . <pm789481@gmail.com>
2026-03-05 17:56:13 -0600

    ingreso y verificacion que no ingresen un producto con la misma clave

---
+++ No.7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> productos = new Dictionary<int, string>();
+string opcion;
+do
+{
+    Console.WriteLine("Ingrese el codigo del producto");
+    int codigo=int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el producto");
+    string productoing=Console.ReadLine();
+    if(productos.ContainsKey(codigo))
+    {
+        Console.WriteLine("Error, el codigo del producto que ingreso ya existe, no se registrara el producto que acaba de ingresar");
+    }
+    else
+    {
+        productos.Add(codigo, productoing);
+    }
+    Console.WriteLine("Desea ingresar otro producto? (s/n)");
+    opcion = Console.ReadLine();
+} while (opcion != "n");
