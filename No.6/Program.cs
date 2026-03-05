b0d9c7e1e9b1860caff7f7b74dc9622f62f4a108
Martinez . <pm789481@gmail.com>
2026-03-05 17:41:00 -0600

    agregar el registro de tres estudiantes

---
+++ No.6/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+estudiantes.Add(22010020, "Manuel");
+estudiantes.Add(23453348, "Sandra");
+estudiantes.Add(23764286, "Paco");
