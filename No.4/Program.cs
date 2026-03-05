2456b7caeb9e4bce16763e60832633094fdd23dd
Martinez . <pm789481@gmail.com>
2026-03-05 16:21:50 -0600

    pedimos el ingreso de la ciudad y el codigo

---
+++ No.4/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> ciudades = new Dictionary<int, string>();
+for(int i=1; i<=5;i++)
+{
+    Console.WriteLine("Ingrese el codigo de la ciudad");
+    int codigo=int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la ciudad");
+    string ciudading=Console.ReadLine();
+    ciudades.Add(codigo,ciudading);
+}
