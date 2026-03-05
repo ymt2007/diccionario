815bda3413c5a64eae213dbaffd22322a438329a
Martinez . <pm789481@gmail.com>
2026-03-05 17:43:57 -0600

    removi el estudiantes 3 y enseñe en pantalla los estudiantes registrados

--- No.6/Program.cs
+++ No.6/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System.Collections.Generic;
 Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+Console.WriteLine("Estudiantes registrados:");
 estudiantes.Add(22010020, "Manuel");
 estudiantes.Add(23453348, "Sandra");
 estudiantes.Add(23764286, "Paco");
+
+estudiantes.Remove(23764286);
+foreach(var est in estudiantes)
+{
+    Console.WriteLine(est);
+}
