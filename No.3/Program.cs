40bf49bbd7f02749d44ecb4ff6de245c8ed4ef97
Martinez . <pm789481@gmail.com>
2026-03-05 15:51:42 -0600

    Ingreso de nombre del estudante, la nota y la nota a buscar

---
+++ No.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+Dictionary<string, double> notas = new Dictionary<string, double>();
+string opcion;
+do
+{
+    Console.WriteLine("Ingrese el nombre del estudiante");
+    string nombre=Console.ReadLine();
+    Console.WriteLine("Ingrese la nota del estudiante");
+    double notaing=double.Parse(Console.ReadLine());
+    Console.WriteLine("Desea ingresar otra nota? (s/n)");
+    opcion=Console.ReadLine();
+} while (opcion != "s");
+Console.Clear();
+Console.WriteLine("Ingrese el nombre del estudiante con la nota a buscar");
+string buscar=Console.ReadLine();
