3d56524e3c7fb34e99c18708ebb659b04e1ae984
Martinez . <pm789481@gmail.com>
2026-03-05 16:43:54 -0600

    buscar y enseñar ciudad con el codigo

--- No.4/Program.cs
+++ No.4/Program.cs
@@ -8,3 +8,20 @@ for(int i=1; i<=5;i++)
     string ciudading=Console.ReadLine();
     ciudades.Add(codigo,ciudading);
 }
+Console.Clear();
+string op;
+do
+{
+    Console.WriteLine("Ingrese el codigo de la ciudad a buscar");
+    int buscar=int.Parse(Console.ReadLine());
+    if(ciudades.ContainsKey(buscar))
+    {
+        Console.WriteLine($"La ciudad con el codigo {buscar} es: {ciudades[buscar]}");
+    }
+    else
+    {
+        Console.WriteLine("No existe ciudad registrada con el codigo ingresado");
+    }
+    Console.WriteLine("Desea buscar otra ciudad? (s/n)");
+    op=Console.ReadLine();
+} while (op != "n");
