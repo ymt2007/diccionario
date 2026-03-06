3178ad5f9ee7af51043060e7b799023cbc83e17f
Martinez . <pm789481@gmail.com>
2026-03-05 18:34:48 -0600

    verificacion que exista el nombre del estudiante

--- No.9/Program.cs
+++ No.9/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 Dictionary<string, double> notas = new Dictionary<string, double>();
-string opcion, nombre;
+string opcion, nombre, op;
 double notaing;
 do
 {
@@ -12,3 +12,19 @@ do
     Console.WriteLine("Desea ingresar otro estudiante? (s/n)");
     opcion = Console.ReadLine();
 } while (opcion != "n");
+Console.Clear();
+do
+{
+    Console.WriteLine("Ingrese el nombre del estudiante del cual desea saber la nota");
+    string buscar = Console.ReadLine();
+    if (notas.ContainsKey(buscar))
+    {
+        Console.WriteLine($"La nota del estudiante {buscar} es: {notas[buscar]}");
+    }
+    else
+    {
+        Console.WriteLine("No se ha registrado a ningun estudiante con ese nombre");
+    }
+    Console.WriteLine("Desea saber la nota de otro estudiante? (s/n)");
+    op= Console.ReadLine();
+} while (op!="n");
