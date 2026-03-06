7baa899e9808c1d0dd298cda58992dc008407793
Martinez . <pm789481@gmail.com>
2026-03-05 18:27:11 -0600

    ingreso de datos

---
+++ No.9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+Dictionary<string, double> notas = new Dictionary<string, double>();
+string opcion, nombre;
+double notaing;
+do
+{
+    Console.WriteLine("Ingrese el nombre del estudiante");
+    nombre = Console.ReadLine();
+    Console.WriteLine("Ingrese la nota del estudiante");
+     notaing = int.Parse(Console.ReadLine());
+    notas.Add(nombre, notaing);
+    Console.WriteLine("Desea ingresar otro estudiante? (s/n)");
+    opcion = Console.ReadLine();
+} while (opcion != "n");
