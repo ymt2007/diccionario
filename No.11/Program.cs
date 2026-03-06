3f88d8a547d091511e8a1bf521541ea09a54ac29
Martinez . <pm789481@gmail.com>
2026-03-06 16:32:53 -0600

    diccionario con key y value

---
+++ No.11/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+estudiantes.Add(123, "Roberto");
+estudiantes.Add(456, "Maria");
+estudiantes.Add(789, "katerin");
+estudiantes.Add(101, "Alejandra");
+estudiantes.Add(121, "Edgar");
+Console.WriteLine("Estudiantes registrados con su respectivo numero de carne");
+foreach(var item in estudiantes)
+{
+    Console.WriteLine(item.Key);
+    Console.WriteLine(item.Value);
+}
