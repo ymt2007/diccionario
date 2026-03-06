d137482027e72949e946ca0f65f5642c0491a190
Martinez . <pm789481@gmail.com>
2026-03-06 16:35:53 -0600

    ingreso de datos

---
+++ No.12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> productos = new Dictionary<int, string>();
+string opcion;
+do
+{
+    Console.WriteLine("Ingrese el codigo del producto");
+    int codigo = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el producto");
+    string productoing = Console.ReadLine();
+    productos.Add(codigo, productoing);
+    Console.WriteLine("Desea ingresar otro producto? (s/n)");
+    opcion = Console.ReadLine();
+} while (opcion != "n");
