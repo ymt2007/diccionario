bb75ecbc7fde942b3fac661b705c50901507b15d
Martinez . <pm789481@gmail.com>
2026-03-06 17:52:45 -0600

    mostrar menu y creacion de case 1

---
+++ No.16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+int opcion;
+do
+{
+    Console.WriteLine("Bienvenido al menu, seleccione la opcion deseada");
+    Console.WriteLine("1. Registrar alumno");
+    Console.WriteLine("2. Mostrar lista de alumnos registrados");
+    Console.WriteLine("3. Salir");
+    opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Ingrese el ID del alumno");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el nombre del alumno");
+            string nombre = Console.ReadLine(); 
+            estudiantes.Add(id, nombre);
+            break;
+    }
+} while (opcion != 3);
