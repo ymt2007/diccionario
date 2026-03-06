6904db71a3c83082460f88e69567081884d83721
Martinez . <pm789481@gmail.com>
2026-03-06 16:49:22 -0600

    ingreso de notas

---
+++ No.13/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+Dictionary<int, double> notas=new Dictionary<int, double>();
+double suma;
+Console.WriteLine("Cuantas notas desea ingresar?");
+int ingreso=int.Parse(Console.ReadLine());
+for(int i=1;i<=ingreso;i++)
+{
+    Console.WriteLine($"Ingrese la nota {i}");
+    double notaing = double.Parse(Console.ReadLine());
+    notas.Add(i, notaing);
+}
