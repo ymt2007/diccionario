d96dbf79ea522f78f94a2e1776158ada9c7800b5
Martinez . <pm789481@gmail.com>
2026-03-05 18:14:28 -0600

    modificacion del nombre y mostrar la lista de los empleados

--- No.8/Program.cs
+++ No.8/Program.cs
@@ -4,3 +4,10 @@ empleados.Add(123, "Juan");
 empleados.Add(456, "Lupe");
 empleados.Add(789, "Maria");
 empleados.Add(101, "Antonia");
+
+empleados[123] = "Pablo";
+Console.WriteLine("Lista de empleados con el nombre modificado");
+foreach(var emp in empleados)
+{
+    Console.WriteLine(emp);
+}
