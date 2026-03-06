8420e56e957f848f51913c8374671234ee111cea
Martinez . <pm789481@gmail.com>
2026-03-05 18:12:00 -0600

    agregar nombres a el diccionario

---
+++ No.8/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> empleados = new Dictionary<int, string>();
+empleados.Add(123, "Juan");
+empleados.Add(456, "Lupe");
+empleados.Add(789, "Maria");
+empleados.Add(101, "Antonia");
