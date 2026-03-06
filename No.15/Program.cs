cb2ebf9f6b6935df414c90fc0118e14d30236572
Martinez . <pm789481@gmail.com>
2026-03-06 17:43:50 -0600

    diccionario con un formato ID, NOMBRE

---
+++ No.15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> empleados = new Dictionary<int, string>();
+string op;
+do
+{
+    Console.WriteLine("Ingrese el ID del usuario");
+    int id = int.Parse(Console.ReadLine());
+    Console.WriteLine("Imgrese el nombre");
+    string nombre = Console.ReadLine();
+    empleados.Add(id, nombre);
+    Console.WriteLine("Desea registrar otro usuario? (s/n)");
+    op = Console.ReadLine();
+} while (op != "n");
+Console.Clear();
+Console.WriteLine("Empleados registrados");
+foreach(var emp in empleados)
+{
+    Console.WriteLine($"ID: {emp.Key} Nombre: {emp.Value}");
+}
