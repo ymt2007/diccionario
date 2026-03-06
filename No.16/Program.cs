764d551bf58c083e082ba02c2735168e18421c72
Martinez . <pm789481@gmail.com>
2026-03-06 17:58:37 -0600

    creacion case 2

--- No.16/Program.cs
+++ No.16/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 Dictionary<int, string> estudiantes = new Dictionary<int, string>();
 int opcion;
+string op;
 do
 {
     Console.WriteLine("Bienvenido al menu, seleccione la opcion deseada");
@@ -11,11 +12,24 @@ do
     switch (opcion)
     {
         case 1:
-            Console.WriteLine("Ingrese el ID del alumno");
-            int id = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese el nombre del alumno");
-            string nombre = Console.ReadLine(); 
-            estudiantes.Add(id, nombre);
+            do
+            {
+                Console.WriteLine("Ingrese el ID del alumno");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el nombre del alumno");
+                string nombre = Console.ReadLine();
+                estudiantes.Add(id, nombre);
+                Console.WriteLine("Desea registrar otro estudiante? (s/n)");
+                op = Console.ReadLine();
+            } while (op != "n");
+            Console.Clear();
             break;
+            case 2:
+            foreach(var est in estudiantes)
+            {
+                Console.WriteLine(est);
+            }
+            break;
+            
     }
 } while (opcion != 3);
