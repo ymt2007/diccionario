32abdc42d21f4f481d794304db1a4f7500e3e481
Martinez . <pm789481@gmail.com>
2026-03-06 17:05:43 -0600

    mostrar solo las claves en una linea

---
+++ No.14/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> ciudades= new Dictionary<int, string>();
+ciudades.Add(1234, "Mexico");
+ciudades.Add(5679, "Guatemala");
+ciudades.Add(9384, "Japon");
+ciudades.Add(2839, "Honduras");
+ciudades.Add(2837, "Portugal");
+foreach(var ciu in ciudades)
+{
+    Console.Write($"{ciu.Key}, ");
+}
