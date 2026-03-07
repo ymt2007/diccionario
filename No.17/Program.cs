b11914261b54317ad9eecd5a2fa5bda15ad357c2
Martinez . <pm789481@gmail.com>
2026-03-06 18:32:00 -0600

    imprimir opciones del menu

---
+++ No.17/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+Dictionary<int, string> productos = new Dictionary<int, string>();
+int op;
+do
+{
+    Console.WriteLine("Binevenido al menu, seleccione la opcion que necesite");
+    Console.WriteLine("1. Ingresar un producto");
+    Console.WriteLine("2. Eliminar un producto por medio de su codigo");
+    Console.WriteLine("3. mostrar productos registrados");
+    Console.WriteLine("4. Salir");
+    op=int.Parse(Console.ReadLine());
+
+} while (op!=4);
