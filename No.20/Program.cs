6871e59f070f7e3de6dbad8d9dbb54999552daf3
Martinez . <pm789481@gmail.com>
2026-03-06 20:55:35 -0600

    diccionario con tupla

---
+++ No.20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+Dictionary < int, (string Nombre, int edad, string carrera) > estudiantes = new Dictionary<int, (string, int, string) > ();
+for(int i=1;i<=3;i++)
+{
+    Console.WriteLine($"Ingrese el carne del estudiante {i}");
+    int carne=int.Parse(Console.ReadLine());
+    Console.WriteLine($"Ingrese el nombre del estudiante {i}");
+    string nombre = Console.ReadLine();
+    Console.WriteLine($"Ingrese la edad del estudiante {i}");
+    int edad = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Ingrese la carrera que cursa el estudiante {i}");
+    string carrera = Console.ReadLine();
+    estudiantes.Add(carne,(nombre,edad,carrera));
+}
+Console.Clear ();
+foreach(var est in estudiantes)
+{
+    Console.WriteLine(est);
+}
