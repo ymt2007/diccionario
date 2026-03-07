4734c9b87f31e2420fe3e4f892766f4bcdbbe69d
Martinez . <pm789481@gmail.com>
2026-03-06 19:06:08 -0600

    imprimir opciones

---
+++ No.19/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+Dictionary<string, double> notas = new Dictionary<string, double>();
+int opcion;
+do
+{
+    Console.WriteLine("Bienvenido al menu, seleccione la opcion deseada");
+    Console.WriteLine("1. Ingresar notas");
+    Console.WriteLine("2. Modificar notas");
+    Console.WriteLine("3. Mostrar notas");
+    Console.WriteLine("4. Salir");
+    opcion = int.Parse(Console.ReadLine());
+
+} while ();
