7a1dbf223cdf09a6dc09f2093e4a9f9a54186636
Martinez . <pm789481@gmail.com>
2026-03-06 18:59:50 -0600

    verificar que el usuario no exista antes de agregarlo al diccionario

---
+++ No.18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+Dictionary<string, string> usuarios = new Dictionary<string, string>();
+string opcion;
+do
+{
+    Console.WriteLine("Ingrese el usuario");
+    string usuarioin = Console.ReadLine();
+    Console.WriteLine("Ingrese la contraseña");
+    string contra= Console.ReadLine();
+    if(usuarios.ContainsKey(usuarioin))
+    {
+        Console.WriteLine("El usuario ya existe, intento de nuevo");
+    }
+    else
+    {
+        usuarios.Add(usuarioin, contra);
+        Console.WriteLine("Usuario registrado con exito");
+    }
+    Console.WriteLine("Desea registrar otro usuario? (s/n)");
+    opcion = Console.ReadLine();
+} while (opcion!="n");
